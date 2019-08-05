15f69af7078e18ef9507bd90838793b852230a8c
apalion1493 <39518411+apalion1493@users.noreply.github.com>
2019-08-05 15:05:05 +0300

    commit 05.08

--- app/js/animation-xl.js
+++ app/js/animation-xl.js
@@ -1,10 +1,32 @@
 var controller = new ScrollMagic.Controller();
 
 //first-pattern
+var tween;
+if (window.innerWidth > 1200) {
 
-var tween = TweenMax.to(".site-templates-section__first-pattern", 1,
-    {left: 0, top: -70},
-    {left: -1800, yoyo: true, ease: Circ.easeInOut});
+    tween = TweenMax.to(".site-templates-section__first-pattern", 1,
+        {left: 0, top: -70},
+        {left: -1800, yoyo: true, ease: Circ.easeInOut});
+
+} else if(window.innerWidth >= 992 && window.innerWidth < 1199.98) {
+
+    tween = TweenMax.to(".site-templates-section__first-pattern", 1,
+        {left: 0, top: -140},
+        {left: -1800, yoyo: true, ease: Circ.easeInOut});
+
+} else if(window.innerWidth >= 768 && window.innerWidth < 991.98) {
+
+    tween = TweenMax.to(".site-templates-section__first-pattern", 1,
+        {left: 0, top: 0},
+        {left: -1800, yoyo: true, ease: Circ.easeInOut});
+
+} else {
+    tween = TweenMax.to(".site-templates-section__first-pattern", 1,
+        {left: 0, top: 0},
+        {left: -1800, yoyo: true, ease: Circ.easeInOut});
+
+}
+
 
 var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 400})
     .setTween(tween)
@@ -14,23 +36,32 @@ var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 400})
 
 //second-pattern
 
-// if (window.innerWidth > 1200 && window.innerWidth < 1600) {
-//
-//
-// } else if(window.innerWidth >= 1600 && window.innerWidth < 1875) {
-//     tween1 = TweenMax.to(".site-templates-section__second-pattern", 1,
-//         {right: 0, top: 280},
-//         {right: -1800, yoyo: true, ease: Circ.easeInOut});
-//
-// } else if(window.innerWidth >= 1876) {
-//     tween1 = TweenMax.to(".site-templates-section__second-pattern", 1,
-//         {right: 0, top: 320},
-//         {right: -1800, yoyo: true, ease: Circ.easeInOut});
-// }
+var tween1;
 
-var tween1 = TweenMax.to(".site-templates-section__second-pattern", 1,
-    {right: 0, top: 225},
-    {right: -1800, yoyo: true, ease: Circ.easeInOut});
+if (window.innerWidth > 1200) {
+
+    tween1 = TweenMax.to(".site-templates-section__second-pattern", 1,
+        {right: 0, top: 225},
+        {right: -1800, yoyo: true, ease: Circ.easeInOut});
+
+} else if(window.innerWidth >= 992 && window.innerWidth < 1199.98) {
+
+    tween1 = TweenMax.to(".site-templates-section__second-pattern", 1,
+        {right: 0, top: 110},
+        {right: -1800, yoyo: true, ease: Circ.easeInOut});
+
+} else if(window.innerWidth >= 768 && window.innerWidth < 991.98) {
+
+    tween1 = TweenMax.to(".site-templates-section__second-pattern", 1,
+        {right: 0, top: 200},
+        {right: -1800, yoyo: true, ease: Circ.easeInOut});
+
+} else {
+    tween1 = TweenMax.to(".site-templates-section__second-pattern", 1,
+        {right: 0, top: 145},
+        {right: -1800, yoyo: true, ease: Circ.easeInOut});
+}
+
 
 var scene1 = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 400})
     .setTween(tween1)
@@ -40,28 +71,51 @@ var scene1 = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 400})
 
 //phone
 
-var tween2 = TweenMax.to(".adaptability-section__phone-img", 0.1,
-    // {right: 0},
+var tween2;
+
+ tween2 = TweenMax.to(".adaptability-section__phone-img", 0.1,
     {opacity: 1},
     {yoyo: true, ease: Circ.easeInOut});
 
 var scene2 = new ScrollMagic.Scene({
-    triggerElement: "#trigger2", duration: 500
+    triggerElement: "#trigger2", duration: 300
 })
     .setTween(tween2)
     .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
 
+
+
+
+
+
+
+
+
 //header
+var tween3;
 
-var tween3 = TweenMax.to(".tablet-images__animate", 0.5,
-    {top: '-300px'}
-    );
+if (window.innerWidth > 1200) {
+    tween3 = TweenMax.to(".tablet-images__animate", 0.5, {top: '-300px'});
+} else if(window.innerWidth >= 992 && window.innerWidth < 1199.98) {
+    tween3 = TweenMax.to(".tablet-images__animate", 0.5, {top: '-300px'});
+} else if(window.innerWidth >= 768 && window.innerWidth < 991.98) {
+    tween3 = TweenMax.to(".tablet-images__animate", 0.5, {top: '-300px'});
+} else {
+    tween3 = TweenMax.to(".tablet-images__animate", 0.5, {top: '-300px'});
+}
 
-var scene3 = new ScrollMagic.Scene({triggerElement: "#trigger", duration: "150%", offset: 420})
+var scene3 = new ScrollMagic.Scene({triggerElement: "#trigger", duration: "100%", offset: 320})
     .setTween(tween3)
     .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
+
+
+
+
+
+
+
 
 //
 
